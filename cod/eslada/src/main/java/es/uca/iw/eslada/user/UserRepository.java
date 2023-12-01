f69be408640b8e6bebfed29cf0abc18870d41d2d
Lazaro Roa Carrillo <lazaro.roacarrillo@alum.uca.es>
2023-12-01 13:28:16 +0100

    Update UserRepository.java

--- cod/eslada/src/main/java/es/uca/iw/eslada/user/UserRepository.java
+++ cod/eslada/src/main/java/es/uca/iw/eslada/user/UserRepository.java
@@ -2,7 +2,12 @@ package es.uca.iw.eslada.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    public List<User>  FindByName(String name);
+    public List<User> FindByNameOrDniAllIgnoreCase(String name, String dni);
+
 }
