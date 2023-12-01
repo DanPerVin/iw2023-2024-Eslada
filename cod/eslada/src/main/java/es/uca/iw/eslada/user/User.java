ba4207ac63f2e24192f539ad81d66f5672014868
Lazaro Roa Carrillo <lazaro.roacarrillo@alum.uca.es>
2023-12-01 12:25:44 +0100

    User Entity creada con métodos

---
+++ cod/eslada/src/main/java/es/uca/iw/eslada/user/User.java
@@ -0,0 +1,85 @@
+package es.uca.iw.eslada.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(length = 100)
+    private String name;
+
+    @Column(length = 9)
+    @Size(min = 9,max = 9)
+    @Pattern(regexp = "^[0-9]{8}[A-Za-z]$")
+    private String dni;
+
+    @Column(length = 100)
+    @Email
+    private String email;
+
+    @NotEmpty
+    @Column(length = 100)
+    private String password;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof User other)) {
+            return false;
+        }
+
+        if (id != null) {
+            return id.equals(other.id);
+        }
+        return super.equals(other);
+    }
+    
+}
