0cb7dd4a2f6ba729f6032b130b4488d9fc3a1c67
Lazaro Roa Carrillo <62333233+Ttgbateverything@users.noreply.github.com>
2023-12-04 17:54:25 +0100

    Campos incluidos en RegisterView
    Campos incluidos hasta botón, sin funcionalidad.

--- cod/eslada/src/main/java/es/uca/iw/eslada/register/RegisterView.java
+++ cod/eslada/src/main/java/es/uca/iw/eslada/register/RegisterView.java
@@ -1,12 +1,15 @@
 package es.uca.iw.eslada.register;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import es.uca.iw.eslada.user.UserService;
 import jdk.jfr.Name;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.button.Button;
 
 import java.awt.*;
 
@@ -14,8 +17,37 @@ import java.awt.*;
 @PageTitle("ESLADA-REGISTER")
 public class RegisterView extends VerticalLayout {
     public RegisterView(@Autowired UserService userService){
-        TextField textField = new TextField("Bienvenido al registro");
+        TextField name = new TextField();
+        TextField dni = new TextField();
+        EmailField email = new EmailField();
+        PasswordField password= new PasswordField();
+        PasswordField password2 = new PasswordField();
+        Button register = new Button("Register");
 
-        add(textField);
+        dni.setPattern("^[0-9]{8}[A-Za-z]$");
+        dni.setMaxLength(9);
+        dni.setMinLength(9);
+        dni.setHelperText("Format: 58723465C");
+
+        password.setMinLength(6);
+        password.setMaxLength(26);
+        password.setHelperText("6-26 characters and numbers");
+
+        password2.setMinLength(6);
+        password2.setMaxLength(26);
+        password2.setHelperText("Passwords must coincide");
+
+        name.setLabel("Name");
+        dni.setLabel("DNI");
+        email.setLabel("e-mail");
+        password.setLabel("Password");
+        password2.setLabel("Repeat Password");
+
+        add(name,dni,email,password,password2,register);
+
+        //register.addClickListener(e -> onRegisterButtonClick());
+    }
+
+    private void onRegisterButtonClick() {
     }
 }
